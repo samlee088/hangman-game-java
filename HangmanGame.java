0cca00929c62b4544453d24020fd9cbc1fd023e3
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-08-16 19:58:33 -0700

    initial game setUP

---
+++ HangmanGame.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class HangmanGame {
+
+
+    public void play() {
+        
+    }
+
+    
+
+    public static void main(String [] args) {
+        HangmanGame game = new HangmanGame();
+        game.play();
+    }
+}
+
